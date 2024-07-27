728d243367c182aa373f10271ea019a72aeafb82
Azmekin <stegancev.d@mail.ru>
2024-07-27 15:40:40 +0300

    feature\add info pullers

--- PowerZVirt.psm1
+++ PowerZVirt.psm1
@@ -1,65 +1,204 @@
 Write-Host "Include ZVIRT Functions and additional variables..." -ForegroundColor Green
 Write-Host "Register base Global Variables..." -ForegroundColor Green
 
-[System.Collections.ArrayList]$global:ZVirt = @()
-
-# Pre-requesites: security, certificates...
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-if ("TrustAllCertsPolicy" -as [type]) {} else {
-
-add-type @"
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-        public bool CheckValidationResult(
-            ServicePoint srvPoint, X509Certificate certificate,
-            WebRequest request, int certificateProblem) {
-            return true;
-        }
-    }
-"@
-  [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-}
-#[Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicyZVirt
+[System.Collections.Generic.Dictionary[string,string]]$global:ZVirt = @{}
 
 
-$global:methods = @{
-    "auth"                         = "/ovirt-engine/sso/oauth/token"
-    #here
-    "getProjects"                  = ":5000/v3/projects?all_tenants=1"
-    "getProjectDetails"            = ":5000/v3/projects/[PROJECT_ID]"
-    "getAllVMs"                    = ":8774/v2.1/servers?all_tenants=1"
-    "getVM"                        = ":8774/v2.1/servers?all_tenants=1&name=[VM_NAME]"
-    "getVMsPerProject"             = ":8774/v2.1/servers?all_tenants=1&project_id=[PROJECT_ID]"
-    "getVMDetails"                 = ":8774/v2.1/servers/[VM_ID]"
-    "getFlavor"                    = ":8774/v2.1/flavors/[FLAVOR_ID]"
-    "getHosts"                     = ":8774/v2.1/os-hypervisors/detail"
-    "getSecurityGroups"            = ":9696/v2.0/security-groups"
-    "getvDisks"                    = ":8776/v2/[PROJECT_ID]/volumes/detail?all_tenants=True&limit=1000"
-    "getStores"                    = ":8776/v2/[PROJECT_ID]/scheduler-stats/get_pools?all_tenants=True&detail=True"
-}
 
-Function GOIDA () {
-    Write-Host "Goida"
-}
-Function Get-ZVirtAuthenticationToken () {
+Function Connect-zVirtController () {
     Param(
         [Parameter(Mandatory=$true)]       [string] $user,
-        [Parameter(Mandatory=$false)]      [string] $domain = "internal",
+        [Parameter(Mandatory=$true)]      [string] $domain,
         [Parameter(Mandatory=$true)]       [string] $secret,
         [Parameter(Mandatory=$true)]       [string] $apiURL
         )
+    $apiURL=$apiURL+"/ovirt-engine/api"
     $b64secret= [convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($user+"@"+$domain+":"+$secret))
-    $result = Invoke-WebRequest -Headers @{"Authorization"="Basic "+$b64secret} -Method Head -Uri $apiURL -UseBasicParsing -Verbose
-    $authToken = $result
-    Write-Host $authToken
-   # $res = @{
-   #     "Content-Type"="application/json"
-   #     "X-Auth-Token" = $result.Headers['X-Subject-Token']
-   #     "X-OpenStack-Nova-API-Version" = "2.27"
-   # }
-    return  $authToken #$res
+    $result = Invoke-WebRequest -Headers @{"Authorization"="Basic "+$b64secret;"Prefer"="persistent-auth"} -Method Head -Uri $apiURL -UseBasicParsing -Verbose -SkipCertificateCheck -SessionVariable websession
+    $cookies = $websession.Cookies.GetCookies($apiURL) 
+    $ZVirt["$apiURL"]=$cookies.Value
+    $RAWData=@{"ZVIRT"=$ZVirt;"RAWData"=$result}
+    return  [pscustomobject]$RAWData
 }
 
+function Get-zVirtConnectedControllers {
+    return  $ZVirt
+}
+
+function Disconnect-zVirtController {
+    param (
+        [Parameter(Mandatory=$true)]       [string] $apiURL
+    )
+    $ZVirt.Remove($apiURL)
+    return  $ZVirt
+}
+
+
+
+function Get-zVirtHosts {
+    param (
+        [Parameter(Mandatory=$false)]       [string] $name
+    )
+    $list=[System.Collections.ArrayList]::new()
+    $RAWData=""
+    if ( $ZVirt.Count -eq 0 ){
+        Write-Error "There is no ZVIRT connections. Create one by Connect-zVirtController"
+    }
+    foreach ($h in $ZVirt.GetEnumerator()){
+        if ($name.Length -ne 0){
+            $ht=$h.Key+"/hosts?search=name%3D"+$name
+        }
+        else {
+            $ht=$h.Key+"/hosts"
+        }
+        $cookie = [System.Net.Cookie]::new('JSESSIONID', $h.Value)
+        $session = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
+        $session.Cookies.Add($h.Key, $cookie)
+        $result = Invoke-WebRequest -Headers @{"Prefer"="persistent-auth";"Accept"="application/json"} -Method Get -Uri $ht -SkipCertificateCheck -UseBasicParsing -Verbose -WebSession $session
+        if ( $result.Count -eq 0 ){
+            Write-Error "Output of request to ZVirtHost is empty"
+        }
+        $RAWData=$result.Content
+        $result=$result | ConvertFrom-Json -AsHashtable
+        foreach($i in $result["host"]){
+
+            $hostinfo=@{"Name" = $i["name"];"ip"=$i["address"];"os"=$i["os"]["version"];"memory"=$i["memory"];"CPU"=$i["cpu"];"serial number"=$i["hardware_information"]["serial_number"];"cluster"=$i["cluster"]["id"];"state"=$i["status"]}
+            [void]$list.Add([pscustomobject]$hostinfo)
+        }
+        $RAWData=@{"RAWData"=$RAWData}
+        [void]$list.Add([pscustomobject]$RAWData)
+
+
+    }
+    return $list
+
+}
+
+function Get-zVirtStores {
+    param (
+        [Parameter(Mandatory=$false)]       [string] $name
+    )
+    $list=[System.Collections.ArrayList]::new()
+    $RAWData=""
+    if ( $ZVirt.Count -eq 0 ){
+        Write-Error "There is no ZVIRT connections. Create one by Connect-zVirtController"
+    }
+    foreach ($h in $ZVirt.GetEnumerator()){
+        if ($name.Length -ne 0){
+            $ht=$h.Key+"/storagedomains?search=name%3D"+$name
+        }
+        else {
+            $ht=$h.Key+"/storagedomains"
+        }
+        $cookie = [System.Net.Cookie]::new('JSESSIONID', $h.Value)
+        $session = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
+        $session.Cookies.Add($h.Key, $cookie)
+        $result = Invoke-WebRequest -Headers @{"Prefer"="persistent-auth";"Accept"="application/json"} -Method Get -Uri $ht -SkipCertificateCheck -UseBasicParsing -Verbose -WebSession $session
+        if ( $result.Count -eq 0 ){
+            Write-Error "Output of request to ZVirtHost is empty"
+        }
+        $RAWData=$result.Content
+                $result=$result | ConvertFrom-Json -AsHashtable
+        foreach($i in $result["storage_domain"]){
+            $total=0
+            try {
+                $total=[Int64]$i["available"]+[int64]$i["used"]
+            }
+            catch {
+                $total=0
+            }
+            $hostinfo=@{"Name" = $i["name"];"type"=$i["storage"]["type"];"total"= $total;"available"=$i["available"];"used"=$i["used"]}
+            [void]$list.Add([pscustomobject]$hostinfo)
+        }
+        $RAWData=@{"RAWData"=$RAWData}
+        [void]$list.Add([pscustomobject]$RAWData)
+
+
+    }
+    return $list
+
+}
+
+function Get-zVirtVMs {
+    param (
+        [Parameter(Mandatory=$false)]       [string] $name,
+        [Parameter(Mandatory=$false)]       [string] $ID
+    )
+    $list=[System.Collections.ArrayList]::new()
+    $RAWData=""
+    if ( $ZVirt.Count -eq 0 ){
+        Write-Error "There is no ZVIRT connections. Create one by Connect-zVirtController"
+    }
+    foreach ($h in $ZVirt.GetEnumerator()){
+        if ($name.Length -ne 0){
+            $ht=$h.Key+"/vms?search=name%3D"+$name+"&follow=disk_attachments.disk,cluster,hostdevices,cpu_profile,nics"
+        }
+        elseif ($ID.Length -ne 0) {
+            $ht=$h.Key+"/vms/"+$ID+"?follow=disk_attachments.disk,cluster,hostdevices,cpu_profile,nics"
+        } 
+        else{
+            $ht=$h.Key+"/vms?follow=disk_attachments.disk,cluster,hostdevices,cpu_profile,nics"
+        }
+        $cookie = [System.Net.Cookie]::new('JSESSIONID', $h.Value)
+        $session = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
+        $session.Cookies.Add($h.Key, $cookie)
+        $result = Invoke-WebRequest -Headers @{"Prefer"="persistent-auth";"Accept"="application/json"} -Method Get -Uri $ht -SkipCertificateCheck -UseBasicParsing -Verbose -WebSession $session
+        if ( $result.Count -eq 0 ){
+            Write-Error "Output of request to ZVirtHost is empty"
+        }
+        $RAWData=$result.Content
+        $result=$result | ConvertFrom-Json -AsHashtable
+        foreach($i in $result["vm"]){
+            Write-Host $i
+            $hostinfo=@{"NameVM" = $i["name"];"description"=$i["description"];"comment"= $i["comment"];"cpu"=$i["cpu"]| ConvertTo-Json;"memory"=$i["memory"];"cluster"=$i["cluster"]| ConvertTo-Json;"disk_attachments"=$i["disk_attachments"]| ConvertTo-Json;"host_devices"=$i["host_devices"]| ConvertTo-Json;"nics"=$i["nics"]| ConvertTo-Json}
+            [void]$list.Add([pscustomobject]$hostinfo)
+        }
+        $RAWData=@{"RAWData"=$RAWData}
+        [void]$list.Add([pscustomobject]$RAWData)
+
+
+    }
+    return $list | Format-List
+
+}
+
+
+function Get-zVirtVMDisks {
+    param (
+        [Parameter(Mandatory=$false)]       [string] $VMName
+    )
+    $list=[System.Collections.ArrayList]::new()
+    $RAWData=""
+    if ( $ZVirt.Count -eq 0 ){
+        Write-Error "There is no ZVIRT connections. Create one by Connect-zVirtController"
+    }
+    foreach ($h in $ZVirt.GetEnumerator()){
+        if ($VMName.Length -ne 0){
+            $ht=$h.Key+"/vms?search=name%3D"+$VMName+"&follow=disk_attachments.disk"
+        }
+        else {
+            $ht=$h.Key+"/vms?follow=disk_attachments.disk"
+        }
+        $cookie = [System.Net.Cookie]::new('JSESSIONID', $h.Value)
+        $session = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
+        $session.Cookies.Add($h.Key, $cookie)
+        $result = Invoke-WebRequest -Headers @{"Prefer"="persistent-auth";"Accept"="application/json"} -Method Get -Uri $ht -SkipCertificateCheck -UseBasicParsing -Verbose -WebSession $session
+        if ( $result.Count -eq 0 ){
+            Write-Error "Output of request to ZVirtHost is empty"
+        }
+        $RAWData=$result.Content 
+        $result=$result | ConvertFrom-Json -AsHashtable
+        foreach($i in $result["vm"]){
+            foreach($j in $i["disk_attachments"]["disk_attachment"]){
+                $diskinfo=@{"NameVM" = $i["name"];"disk_size" = $j["disk"]["actual_size"];"storage_type"=$j["disk"]["storage_type"];"status"=$j["disk"]["status"]}
+            }
+            [void]$list.Add([pscustomobject]$diskinfo)
+        }
+        $RAWData=@{"RAWData"=$RAWData}
+        [void]$list.Add([pscustomobject]$RAWData)
+
+
+    }
+    return $list | Format-List
+
+}
